91ee43142c4bce18bf58d64fa6bd59aa2cdbd015
NazarMaestro <81748904+NazarMaestro@users.noreply.github.com>
2021-04-25 01:20:24 +0300

    Add files via upload

--- components/ChangeDay.js
+++ components/ChangeDay.js
@@ -3,11 +3,15 @@ import { StyleSheet, View,Text } from 'react-native'
 import Icon from 'react-native-vector-icons/AntDesign'
 
 const ChangeDay = () => {
+    var date = new Date();
+    var m = date.getMonth();m++;
+    m  < 10 ? m ='0'+ m : m;
+    let array = ['Воскресенье','Понедельник','Вторник','Среда','Четвер','Пятница','Суббота']
     return (
         <View style={styles.container}>
             <Text style={styles.text}>
             <Icon name='left' color='blue' size={30} style={{paddingRight:15}}/>      
-            День тижня      
+             {array[date.getDay()] + ', ' + date.getDate()+'-'+m+'-' + date.getFullYear()}
             <Icon name='right' color='blue' size={30} style={{paddingLeft:15}}/>
             </Text>
         </View>
