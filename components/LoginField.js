f086836455a95ba5b5726c206f51a7e2415f0bc5
k3r14 <kravchenko230501@gmail.com>
2021-06-13 20:41:12 +0300

    fix this

--- components/LoginField.js
+++ components/LoginField.js
@@ -3,10 +3,10 @@ import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Logo from '../assets/logo.svg'
-import {useState} from 'react'
+import { useState } from 'react'
 const styles = StyleSheet.create({
     text: {
-        letterSpacing: '3px',
+        letterSpacing: 3,
         paddingTop: '20px',
         paddingLeft: '25px',
         paddingRight: '20px',
@@ -21,30 +21,32 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     border: {
-        width: 'min(max(15vw + 100px, 150vh - 70vw), 90vw)',
-        background: '#FCFCFC',
-        border: '2px solid #0872B9',
-        borderRadius: '10px',
+        // width: 'min(max(15vw + 100px, 150vh - 70vw), 90vw)',
+        backgroundColor: '#FCFCFC',
+        borderWidth: 2,
+        borderStyle: 'solid',
+        borderColor: '#0872B9',
+        borderRadius: 10,
         height: '70px'
     },
     image: {
-        width: 'min(max(30vw, 150vh - 60vw), 90vw)',
-        aspectRatio: '1350 / 342'
+        // width: 'min(max(30vw, 150vh - 60vw), 90vw)',
+        aspectRatio: 1350 / 342
     },
     gap1: {
-        height: '10vh'
+        // height: '10vh'
     },
     gap2: {
-        height: '4vh'
+        // height: '4vh'
     },
     button: {
         backgroundColor: '#3159B4',
-        borderRadius: '10px',
-        width: 'min(max(7vw + 50px, 80vh - 50vw), 50vw)',
+        borderRadius: 10,
+        // width: 'min(max(7vw + 50px, 80vh - 50vw), 50vw)',
         height: '60px'
     },
     textbutton: {
-        letterSpacing: '2.5px',
+        letterSpacing: 2.5,
         paddingTop: '18px',
         textAlign: 'center',
         fontSize: 18,
@@ -56,31 +58,31 @@ const styles = StyleSheet.create({
 const LoginField = (props) => {
 
     let TStyle = {
-        width: 'min(max(15vw + 100px, 150vh - 70vw), 90vw)',
+        // width: 'min(max(15vw + 100px, 150vh - 70vw), 90vw)',
     };
     let BStyle = {
         backgroundColor: '#3159B4',
-        borderRadius: '10px',
-        width: 'min(max(7vw + 50px, 80vh - 50vw), 50vw)',
+        borderRadius: 10,
+        // width: 'min(max(7vw + 50px, 80vh - 50vw), 50vw)',
         height: '60px',
         textAlign: 'center',
         fontWeight: '400',
         color: '#F7F7FC',
     }
     let [loginPayload, setLoginPayload] = useState({});
-    let sumbitFuction = () =>{
+    let sumbitFuction = () => {
         props.login(loginPayload)
     }
     return (
         <View style={styles.container}>
-            <Image style = {styles.image} source = {Logo}/>
-            <View style = {styles.gap1}/>
-            <TextField style = {TStyle} label="email" variant="outlined"
-             onChange={()=>setLoginPayload({...loginPayload, email: event.target.value})}> email </TextField>
-             <TextField style = {TStyle} label="Номер телефону" variant="outlined"
-             onChange={()=>setLoginPayload({...loginPayload, password: event.target.value})}> Пароль </TextField>
-            <View style = {styles.gap2}/>
-            <Button style = {BStyle} onClick={()=>sumbitFuction()}> Авторизация</Button>
+            <Image style={styles.image} source={Logo} />
+            <View style={styles.gap1} />
+            <TextField style={TStyle} label="email" variant="outlined"
+                onChange={() => setLoginPayload({ ...loginPayload, email: event.target.value })}> email </TextField>
+            <TextField style={TStyle} label="Пароль" variant="outlined"
+                onChange={() => setLoginPayload({ ...loginPayload, password: event.target.value })}> Пароль </TextField>
+            <View style={styles.gap2} />
+            <Button style={BStyle} onClick={() => sumbitFuction()}> Авторизация</Button>
         </View>
     );
 }
--- components/RegistrationField.js
+++ components/RegistrationField.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import {useState} from 'react'
+import { useState } from 'react'
 
 const styles = StyleSheet.create({
     text: {
-        letterSpacing: '3px',
+        letterSpacing: 3,
         paddingTop: '20px',
         paddingLeft: '25px',
         paddingRight: '20px',
@@ -21,23 +21,25 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     border: {
-        width: 'min(max(15vw + 100px, 150vh - 70vw), 90vw)',
-        background: '#FCFCFC',
-        border: '2px solid #0872B9',
-        borderRadius: '10px',
+        // width: 'min(max(15vw + 100px, 150vh - 70vw), 90vw)',
+        backgroundColor: '#FCFCFC',
+        borderWidth: 2,
+        borderStyle: 'solid',
+        borderColor: '#0872B9',
+        borderRadius: 10,
         height: '70px'
     },
     image: {
-        width: 'min(max(30vw, 150vh - 60vw), 90vw)',
-        aspectRatio: '1350 / 342'
+        // width: 'min(max(30vw, 150vh - 60vw), 90vw)',
+        aspectRatio: 1350 / 342
     },
     gap2: {
-        height: '2vh'
+        // height: '2vh'
     },
     button: {
         backgroundColor: '#3159B4',
-        borderRadius: '10px',
-        width: 'min(max(7vw + 50px, 80vh - 50vw), 50vw)',
+        borderRadius: 10,
+        // width: 'min(max(7vw + 50px, 80vh - 50vw), 50vw)',
         height: '60px'
     },
     textheader: {
@@ -55,7 +57,7 @@ const RegistrationField = (props) => {
     };
     let BStyle = {
         backgroundColor: '#3159B4',
-        borderRadius: '10px',
+        borderRadius: 10,
         width: 'min(max(8vw + 80px, 80vh - 50vw), 50vw)',
         height: '60px',
         textAlign: 'center',
@@ -63,22 +65,22 @@ const RegistrationField = (props) => {
         color: '#F7F7FC',
     }
     let [registrationPayload, setRegistrationPayload] = useState({});
-    let sumbitFuction = () =>{
+    let sumbitFuction = () => {
         props.registration(registrationPayload)
     }
     return (
         <View style={styles.container}>
-            <View style = {styles.gap1}/>
-            <Text style = {styles.textheader}>Регистрация</Text>
-            <View style = {styles.gap2}/>
-            <TextField style = {TStyle} label="Имя" variant="outlined" onChange={()=>setRegistrationPayload({...registrationPayload, firstName: event.target.value})}> Имя </TextField>
-            <TextField style = {TStyle} label="Фамилия" variant="outlined" onChange={()=>setRegistrationPayload({...registrationPayload, lastName: event.target.value})}> Фамилия </TextField>
-            <TextField style = {TStyle} label="Отчество" variant="outlined" onChange={()=>setRegistrationPayload({...registrationPayload, secondName: event.target.value})}> Отчество </TextField>
-            <TextField style = {TStyle} label="Email" variant="outlined" onChange={()=>setRegistrationPayload({...registrationPayload, email: event.target.value})}> email </TextField>
-            <TextField style = {TStyle} label="Телефон" variant="outlined" onChange={()=>setRegistrationPayload({...registrationPayload, phone: event.target.value})}> Номер телефону </TextField>
-            <TextField style = {TStyle} label="Пароль" variant="outlined" onChange={()=>setRegistrationPayload({...registrationPayload, password: event.target.value})}> Пароль </TextField>
-            <View style = {styles.gap2}/>
-            <Button style = {BStyle} onClick={()=>sumbitFuction()}>Подтвердить</Button>
+            <View style={styles.gap1} />
+            <Text style={styles.textheader}>Регистрация</Text>
+            <View style={styles.gap2} />
+            <TextField style={TStyle} label="Имя" variant="outlined" onChange={() => setRegistrationPayload({ ...registrationPayload, firstName: event.target.value })}> Имя </TextField>
+            <TextField style={TStyle} label="Фамилия" variant="outlined" onChange={() => setRegistrationPayload({ ...registrationPayload, lastName: event.target.value })}> Фамилия </TextField>
+            <TextField style={TStyle} label="Отчество" variant="outlined" onChange={() => setRegistrationPayload({ ...registrationPayload, secondName: event.target.value })}> Отчество </TextField>
+            <TextField style={TStyle} label="Email" variant="outlined" onChange={() => setRegistrationPayload({ ...registrationPayload, email: event.target.value })}> email </TextField>
+            <TextField style={TStyle} label="Телефон" variant="outlined" onChange={() => setRegistrationPayload({ ...registrationPayload, phone: event.target.value })}> Номер телефону </TextField>
+            <TextField style={TStyle} label="Пароль" variant="outlined" onChange={() => setRegistrationPayload({ ...registrationPayload, password: event.target.value })}> Пароль </TextField>
+            <View style={styles.gap2} />
+            <Button style={BStyle} onClick={() => sumbitFuction()}>Подтвердить</Button>
         </View>
     );
 }
